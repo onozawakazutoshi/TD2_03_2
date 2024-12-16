536c2222b4588886ce6c12228bd9891d845c5b50
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-12-16 12:22:53 +0900

    SPACEでタイトル、ゲーム、クリア遷移移動可能

--- Scene.cpp
+++ Scene.cpp
@@ -2,15 +2,61 @@
 
 void Scene::Initialize() {
 	gameMap = new GameMap();
+	player = new Player();
 	gameMap->Initialize();
+	player->Initialize();
 
 }
 
-void Scene::Update() {
-	gameMap->Update();
+void Scene::Update(const char* keys, const char* prekeys){
+    switch (currentState){
+    case SceneState::Title:
 
+        if (keys[DIK_SPACE] && !prekeys[DIK_SPACE]) {
+            currentState = SceneState::Game;
+        }
+
+        break;
+    case SceneState::Game:
+
+        gameMap->Update();
+        player->Update();
+
+        if (keys[DIK_SPACE] && !prekeys[DIK_SPACE]) {
+            currentState = SceneState::Clear;
+        }
+
+        break;
+    case SceneState::Clear:
+
+        if (keys[DIK_SPACE] && !prekeys[DIK_SPACE]) {
+            currentState = SceneState::Title;
+
+        }
+        break;
+    }
 }
 
 void Scene::Draw() {
-	gameMap->Draw();
+    switch (currentState)
+    {
+    case SceneState::Title: 
+        Novice::ScreenPrintf(0,0,"Scene : Title");
+
+
+        break;
+    case SceneState::Game:
+        Novice::ScreenPrintf(0, 0, "Scene : Game");
+
+
+        gameMap->Draw();
+        player->Draw();
+
+        break;
+    case SceneState::Clear:
+        Novice::ScreenPrintf(0, 0, "Scene : Clear");
+
+
+        break;
+    }
 }
--- Scene.h
+++ Scene.h
@@ -1,19 +1,31 @@
 #pragma once
 #include"GameMap.h"
+#include"Player.h"
+#include <Novice.h>
 
 // シーンクラス
 class Scene
 {
 public: // メンバ関数
+	// シーンの状態を管理する列挙型
+	enum class SceneState
+	{
+		Title,
+		Game,
+		Clear
+	};
+
 	// 初期化
 	void Initialize();
 	// 更新処理
-	void Update();
+	void Update(const char* keys,const char* prekeys);
 	// 描画処理
 	void Draw();
 
 private: // メンバ変数
+	// シーンの現在の状態
+	SceneState currentState = SceneState::Title;
 	// ポインタ
 	GameMap* gameMap = nullptr;
-
+	Player* player = nullptr;
 };
